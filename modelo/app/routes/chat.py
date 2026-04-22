28cea99f6d3c4f03e697e9ccd2a9c062d5bada7a
samuelsalcedo-ia <ssalcedodaw@gmail.com>
2026-04-22 23:08:30 +0200

    feat: Guardar consultas en RDS e integrar CloudWatch (#76)

--- modelo/app/routes/chat.py
+++ modelo/app/routes/chat.py
@@ -2,6 +2,8 @@ from flask import Blueprint, Response, request
 import ollama
 import modelo
 import toxic_class
+import threading
+from .aws_services import send_metric_to_cloudwatch, save_query_to_rds
 
 chat = Blueprint("chat", __name__)
 contexto = modelo.Contexto()
@@ -13,6 +15,10 @@ def ollamer():
     messages = convers["messages"]
     
     query = messages[-1]['content']
+    
+    # [INYECCIÓN 1]: Registrar métrica de uso (asíncrono)
+    threading.Thread(target=send_metric_to_cloudwatch, args=("ChatRequestCount",)).start()
+
     if(toxic.predict(query)):
         return  Response(f"data: Esta pregunta contiene lenguaje ofensivo, por favor abstente de incluir insultos o lenguaje ofensivo \n\n", content_type='text/event-stream')
     contx = contexto.pasenContexto(query=query)
@@ -30,8 +36,13 @@ def ollamer():
             think=None
         )
 
+        full_response = ""
         for chunk in stream:
             content = chunk['message']['content']
+            full_response += content
             yield f"data: {content}\n\n"
+            
+        # [INYECCIÓN 2]: Guardar historial al finalizar (asíncrono)
+        threading.Thread(target=save_query_to_rds, args=(query, full_response)).start()
 
     return Response(generate(), content_type='text/event-stream')
